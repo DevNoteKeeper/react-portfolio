0b79069bbeaacd0c4511fdc84da719889e110197
DevNoteKeeper <147824845+DevNoteKeeper@users.noreply.github.com>
2025-06-15 16:03:08 +0200

    Update ImageBase.js

--- src/constants/ImageBase.js
+++ src/constants/ImageBase.js
@@ -1,8 +1,8 @@
 export const IMAGE_BASE = {
-    development: 'https://raw.githubusercontent.com/DevNoteKeeper/Portfolio/main/src/assets/logo/development/',
-    design: 'https://raw.githubusercontent.com/DevNoteKeeper/Portfolio/main/src/assets/logo/design/',
-    nocode: 'https://raw.githubusercontent.com/DevNoteKeeper/Portfolio/main/src/assets/logo/nocode/',
-    community: 'https://raw.githubusercontent.com/DevNoteKeeper/Portfolio/main/src/assets/logo/tool/',
-    vsc: 'https://raw.githubusercontent.com/DevNoteKeeper/Portfolio/refs/heads/main/src/assets/logo/vcs/',
+    development: 'https://raw.githubusercontent.com/DevNoteKeeper/dev_hub_imgae/main/development/',
+    design: 'https://raw.githubusercontent.com/DevNoteKeeper/dev_hub_imgae/main/design/',
+    nocode: 'https://raw.githubusercontent.com/DevNoteKeeper/dev_hub_imgae/main/nocode/',
+    community: 'https://raw.githubusercontent.com/DevNoteKeeper/dev_hub_imgae/main/tool/',
+    vcs: 'https://raw.githubusercontent.com/DevNoteKeeper/dev_hub_imgae/main/vcs/',
 
 }
