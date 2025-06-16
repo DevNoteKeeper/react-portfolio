1fcf19ff64485497e2a7038f540dc122e201d2db
DevNoteKeeper <seoyoon.developer@gmail.com>
2025-06-16 16:04:54 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import ProjectsPage from "./pages/ProjectsPage";
 
-
-
-function App(){
-    return (
-        <Router>
-            <Routes>
-                <Route path ="/" element={<MainPage />} />
-                <Route path ="/projects" element={<ProjectsPage />} />
-            </Routes>
-        </Router>
-    );
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+      </Routes>
+    </Router>
+  );
 }
 export default App;
